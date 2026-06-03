26d4aa2ad5b9f57672ebb75d79b6ab269fcc4ae9
Fabiola Burgos <fburgos913@gmail.com>
2026-06-03 01:13:34 -0700

    Fix FizzBuzz logic order

---
+++ challenge/0-fizzbuzz.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+FizzBuzz implementation
+"""
+
+
+def fizzbuzz(n):
+    result = []
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            result.append("FizzBuzz")
+        elif i % 3 == 0:
+            result.append("Fizz")
+        elif i % 5 == 0:
+            result.append("Buzz")
+        else:
+            result.append(str(i))
+    print(" ".join(result))
+
+
+if __name__ == "__main__":
+    import sys
+
+    fizzbuzz(int(sys.argv[1]))
