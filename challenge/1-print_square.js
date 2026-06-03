98b1770999cff2c18b90fc94373568e58b2a1b9c
Fabiola Burgos <fburgos913@gmail.com>
2026-06-03 01:18:20 -0700

    Fix square printing logic in JS

---
+++ challenge/1-print_square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+/*
+  Prints a square of size specified by argument
+*/
+
+const size = parseInt(process.argv[2]);
+
+if (isNaN(size)) {
+  console.log("Missing size");
+} else {
+  for (let i = 0; i < size; i++) {
+    let line = "";
+    for (let j = 0; j < size; j++) {
+      line += "#";
+    }
+    console.log(line);
+  }
+}
